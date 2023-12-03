f9d995bb77115c9b30aaabd4efbf8ec0bc50a2be
tenderconflict <127230706+tenderconflict@users.noreply.github.com>
2023-12-03 16:50:04 -0500

    Changing visuals for logged in page
    indicates the user they are logged in and allows them to return home

--- LoginSystemChurch.js
+++ LoginSystemChurch.js
@@ -1,12 +1,17 @@
 document.getElementById('loginForm').addEventListener('submit', function(event) {
     event.preventDefault();
+    //gathering both fields
     let username = document.getElementById('username').value;
     let password = document.getElementById('password').value;
 
+//the below stub immitates a functional login system that we would have in place where
+//the script would verify with the database that the credentials match the ones inputted
+//all login information would be heavily encrypted.
 
-//if the username matches the hardcoded one below then the user is granted access to
+
+//if the username matches the stub one below then the user is granted access to
 //the test login page
-    if (username === 'admin' && password === 'password') {
+    if (username === 'churchUser' && password === 'password') {
         
         window.location.href = 'testloggedinpage.html'; 
     } else {
